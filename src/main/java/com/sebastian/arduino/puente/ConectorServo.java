7f36fc4f61065cdffc95e77d317957b43aa4d2a2
Sebastián Ávila A <sebastian4j@gmail.com>
2020-01-03 21:46:57 -0300

    lectura de lo que envía el arduino

--- src/main/java/com/sebastian/arduino/puente/ConectorServo.java
+++ src/main/java/com/sebastian/arduino/puente/ConectorServo.java
@@ -1,6 +1,8 @@
 package com.sebastian.arduino.puente;
 
 import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
 import java.io.IOException;
 
 /**
@@ -10,42 +12,57 @@ import java.io.IOException;
  */
 public final class ConectorServo {
 
-  private static SerialPort SP;
-  private static boolean iniciado = false;
-  
-  private static ConectorServo cs;
-  
-  public static synchronized ConectorServo instance() {
-      if (cs == null) {
-          cs = new ConectorServo();
-      }
-      iniciar();
-      return cs;
-  }
+    private static SerialPort SP;
+    private static boolean iniciado = false;
 
-  private static void iniciar() {
-    if (!iniciado) {
-      iniciado = true;
-      SP = SerialPort.getCommPort("/dev/ttyACM0");
-      SP.setComPortParameters(9600, 8, 1, 0); 
-      SP.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0);
-      SP.openPort();
+    private static ConectorServo cs;
+
+    public static synchronized ConectorServo instance() {
+        if (cs == null) {
+            cs = new ConectorServo();
+        }
+        iniciar();
+        return cs;
     }
-  }
 
-  private ConectorServo() {}
-  
+    private static void iniciar() {
+        if (!iniciado) {
+            iniciado = true;
+            SP = SerialPort.getCommPort("/dev/ttyACM0");
+            SP.setComPortParameters(9600, 8, 1, 0);
+            SP.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 0, 0);
+            SP.openPort();
+            SP.addDataListener(new SerialPortDataListener() {
+                @Override
+                public int getListeningEvents() {
+                    return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+                }
 
-  public void subirPuente() throws IOException {
-    SP.getOutputStream().write(1);
-  }
+                @Override
+                public void serialEvent(SerialPortEvent event) {
+                    if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE) {
+                        return;
+                    }
+                    byte[] newData = new byte[SP.bytesAvailable()];
+                    int numRead = SP.readBytes(newData, newData.length);
+                }
+            });
+        }
+    }
 
-  public void bajarPuente() throws IOException {
-    SP.getOutputStream().write(2);
-  }
+    private ConectorServo() {
+    }
 
-  public void cerrar() {
-    SP.closePort();
-  }
+    public void subirPuente() throws IOException {
+        SP.getOutputStream().write(1);
+    }
+
+    public void bajarPuente() throws IOException {
+        SP.getOutputStream().write(2);
+    }
+
+    public void cerrar() {
+        SP.closePort();
+    }
 
 }
